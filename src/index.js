5fe8e68b514c32d35f1b0582df47c2d60316cd0a
Yury Staravoitau <MorfinAll@mail.ru>
2018-03-02 06:24:07 +0300

    finish task. All tests passed.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let five = 0;
+  for (let i = 1; i <= number; i++) {
+    let tempNumber = i;
+   // console.log("i=      " + i);
+    while (tempNumber % 5 === 0) {
+   //   console.log(tempNumber);
+      five++;
+      tempNumber = tempNumber / 5;
+    }
+
+  }
+  let zeroes = five;
+  return zeroes;
 }
